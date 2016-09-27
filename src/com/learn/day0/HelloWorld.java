266eaab306803d30c98ea427085a2b77c66daa0a
Rishi <rishi.h2kinfosys@gmail.com>
2016-09-27 17:36:47 +0530

    Hello World First Program

---
+++ src/com/learn/day0/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.learn.day0;
+
+public class HelloWorld {
+
+	// public static void main is entry point method for Java Program
+	public static void main(String[] args) {
+
+		System.out.println("Hi System, Use your output device (monitor) to print a line ::");
+		
+		System.out.println("Hello World!!!");
+
+	}
+
+}
