50fb1ed1d4c41fd0b0aea23bf4915c8e7b46fac9
Rishi <rishi.h2kinfosys@gmail.com>
2016-09-29 22:56:45 -0400

    Test Commit for Learning GIT

--- src/com/learn/day0/HelloWorld.java
+++ src/com/learn/day0/HelloWorld.java
@@ -7,7 +7,7 @@ public class HelloWorld {
 
 		System.out.println("Hi System, Use your output device (monitor) to print a line ::");
 		
-		System.out.println("Hello World!!!");
+		System.out.println("Hello Students!!!");
 
 	}
 
---
+++ src/com/learn/day0/PrintMyEmail.java
@@ -0,0 +1,10 @@
+package com.learn.day0;
+
+public class PrintMyEmail {
+
+	public static void main(String[] args) {
+		System.out.println("My Email Address is :: rishi.h2kinfosys@gmail.com");
+
+	}
+
+}
